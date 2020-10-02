0dc72fc7c84dd0481cce8910af243cee1036c4e7
Paul Stevens <twinkisan@hotmail.com>
2020-10-01 17:39:39 -0700

    refactored, add tests, added reset button

--- r-p-s.js
+++ r-p-s.js
@@ -1,5 +1,6 @@
 // import functions and grab DOM elements
 import { getRandomThrow } from './get-random-throw.js';
+import { checkResults } from './check-results.js';
 
 const button = document.querySelector('#submit-button');
 const resultsSpan = document.querySelector('#results');
@@ -7,12 +8,14 @@ const winsSpan = document.querySelector('#wins');
 const lossesSpan = document.querySelector('#losses');
 const drawsSpan = document.querySelector('#draws');
 const percentSpan = document.querySelector('#percent');
+const resetButton = document.querySelector('#reset-button');
 
-// initialize state
+
 let wins = 0;
 let losses = 0;
 let draws = 0;
 let total = 0;
+
 
 // set event listeners to update state and DOM
 
@@ -21,63 +24,41 @@ button.addEventListener('click', () => {
     const checkedButton = document.querySelector(':checked');
     const userInput = checkedButton.value;
     const computerInput = getRandomThrow();
-
-    if (userInput === 'rock' && computerInput === 'paper') {
-        losses++;
-        total++;
-
-        resultsSpan.textContent = 'Paper covers rock, you lose!';
-        lossesSpan.textContent = losses;
-        percentSpan.textContent = `${(wins / total) * 100}%`;
-    
-    } else if (userInput === 'paper' && computerInput === 'scissors') {
-        losses++;
-        total++;
-
-        resultsSpan.textContent = 'Scissors cuts paper, you lose!';
-        lossesSpan.textContent = losses;
-        percentSpan.textContent = `${(wins / total) * 100}%`;
-    
-    } else if (userInput === 'scissors' && computerInput === 'rock') {
-        losses++;
-        total++;
-
-        resultsSpan.textContent = 'Rock smashes scissors, you lose!';
-        lossesSpan.textContent = losses;
-        percentSpan.textContent = `${(wins / total) * 100}%`;
-    
-    } else if (userInput === 'rock' && computerInput === 'scissors') {
+    const result = checkResults(userInput, computerInput);
+    if (result === 'win') {
         wins++;
-        total++;
+        resultsSpan.textContent = 'You won!';
+    } else if (result === 'lose') {
+        losses++;
+        resultsSpan.textContent = 'You lose!';
 
-        resultsSpan.textContent = 'Rock smashes scissors, you win!';
-        winsSpan.textContent = wins;
-        percentSpan.textContent = `${(wins / total) * 100}%`;
-    
-    } else if (userInput === 'paper' && computerInput === 'rock') {
-        wins++;
-        total++;
-
-        resultsSpan.textContent = 'Paper covers rock, you win!';
-        winsSpan.textContent = wins;
-        percentSpan.textContent = `${(wins / total) * 100}%`;
-    
-    } else if (userInput === 'scissors' && computerInput === 'paper') {
-        wins++;
-        total++;
-
-        resultsSpan.textContent = 'Scissors cut paper, you win!';
-        winsSpan.textContent = wins;
-        percentSpan.textContent = `${(wins / total) * 100}%`;
-    
     } else {
         draws++;
-        total++;
-
-        resultsSpan.textContent = 'Draw!';
-        drawsSpan.textContent = draws;
-        percentSpan.textContent = `${(wins / total) * 100}%`;
+        resultsSpan.textContent = 'Its a draw!';
 
     }
-    
+
+    winsSpan.textContent = wins;
+    lossesSpan.textContent = losses;
+    drawsSpan.textContent = draws;
+
+    total++;
+    percentSpan.textContent = `${((wins / total) * 100).toFixed(1)}%`;
+
 });
+
+resetButton.addEventListener("click", () => {
+    wins = 0;
+    losses = 0;
+    draws = 0;
+    total = 0;
+    percentSpan.textContent = "";
+    winsSpan.textContent = wins;
+    lossesSpan.textContent = losses;
+    drawsSpan.textContent = draws;
+
+});
+
+// document.getElementById("reset").onclick = function() {
+//     document.getElementById("outcomes").value = "";
+// }
